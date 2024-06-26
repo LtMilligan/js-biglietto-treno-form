8a4ba93a53fb0568d4284509be714897bf534023
LtMilligan <gioele.miscia@gmail.com>
2024-06-26 16:32:59 +0200

    [UPD] Created const name

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const btn = document.getElementById('calcola')
 btn.addEventListener('click', function () {
     const name = document.getElementById('nome').value;
-   
+    const km = document.getElementById('km').value;
+    
     
 });
