d0ffbe9eb9015f25bb457cbbbcfb39a65b9a2d91
LtMilligan <gioele.miscia@gmail.com>
2024-06-26 16:33:54 +0200

    [UPD] Created let discount with if

--- js/script.js
+++ js/script.js
@@ -3,6 +3,12 @@ btn.addEventListener('click', function () {
     const name = document.getElementById('nome').value;
     const km = document.getElementById('km').value;
     const age = document.getElementById('eta').value
+    let discount = 0
+    if (age === 'minorenne') {
+        discount = 0.2
+    }else if (age === 'over65') {
+        discount = 0.4
+    }
     
     
 });
