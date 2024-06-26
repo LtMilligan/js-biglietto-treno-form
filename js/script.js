41ba34b8279c258e6373726007c202ce0087a671
LtMilligan <gioele.miscia@gmail.com>
2024-06-26 16:33:23 +0200

    [UPD] Created const age

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ const btn = document.getElementById('calcola')
 btn.addEventListener('click', function () {
     const name = document.getElementById('nome').value;
     const km = document.getElementById('km').value;
+    const age = document.getElementById('eta').value
     
     
 });
